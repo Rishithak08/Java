b94a75a688f02cb2444068584c92f2ae493c2bb2
Rishithak08 <rishithareddy5353@gmail.com>
2023-06-24 21:54:41 -0500

    assignment4

---
+++ Assignment4.java
@@ -0,0 +1,14 @@
+public class Assignment4 {
+    public static void main(String[] args) {
+        int i = 20;
+        while(i<=30){
+            if(i%2==0){
+                System.out.println("even number "+i);
+            }
+            else{
+                System.out.println("odd number "+i);
+            }
+            i++;
+        }
+    }
+}
---
+++ Assignment4B.java
@@ -0,0 +1,9 @@
+public class Assignment4B {
+    public static void main(String[] args) {
+        int a=5;
+        for(int i=0;i<=10;i++){
+            System.out.println(a+" * "+i+" = "+a*i);
+        }
+
+    }
+}
